28bb4248ce6e4c5f4c59f7582762b5a291fbd709
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-09-07 16:34:32 -0300

    Obtendo um Post por id

--- src/main/java/com/jasonsilvestre/workshopmongo/domain/Post.java
+++ src/main/java/com/jasonsilvestre/workshopmongo/domain/Post.java
@@ -30,6 +30,38 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     public AuthorDTO getAuthor() {
         return author;
     }
---
+++ src/main/java/com/jasonsilvestre/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.jasonsilvestre.workshopmongo.services;
+
+import com.jasonsilvestre.workshopmongo.domain.Post;
+import com.jasonsilvestre.workshopmongo.domain.User;
+import com.jasonsilvestre.workshopmongo.repository.PostRepository;
+import com.jasonsilvestre.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
